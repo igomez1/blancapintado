3d09f9220dd80de5dcb2ff791728379587ea3904
Your Name <you@example.com>
2017-07-25 11:21:53 -0400

    email

--- email.php
+++ email.php
@@ -26,7 +26,7 @@ $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
 
-$to = "joyceigomez@gmail.com";//<== update the email address
+$to = "joyce@joyceigomez@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
